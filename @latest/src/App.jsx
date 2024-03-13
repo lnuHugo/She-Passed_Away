566cbd022c85b898a36d1b9c7ef15bc7bccdb241
G-O Jacobsson <geo.jacobsson@gmail.com>
2024-03-13 14:14:02 +0100

    Add header/footer

--- @latest/src/App.jsx
+++ @latest/src/App.jsx
@@ -1,8 +1,16 @@
-import { useState, useEffect } from "react";
-import "./App.css";
+import './App.css';
+import './Header.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+
+      <Footer />
+    </>
+  );
 }
 
 export default App;
